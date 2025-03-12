0b784a90d51313fbc0d57a230507f637213fe7b4
simondoku <sdoku@stetson.edu>
2025-03-12 02:51:44 -0400

    Updates

---
+++ precache-manifest.d45e80c2e9a8501de00ff7134f736b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37eb273679824b696adb3ffc01c12eb",
+    "url": "/Work-Reminder-App/index.html"
+  },
+  {
+    "revision": "d5226bcebd7ed3750f4a",
+    "url": "/Work-Reminder-App/static/css/main.8d4022c3.chunk.css"
+  },
+  {
+    "revision": "704ba59d954a7089f5ae",
+    "url": "/Work-Reminder-App/static/js/2.fe5d7315.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Work-Reminder-App/static/js/2.fe5d7315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5226bcebd7ed3750f4a",
+    "url": "/Work-Reminder-App/static/js/main.61024d39.chunk.js"
+  },
+  {
+    "revision": "e9964b2a00d37bd9a395",
+    "url": "/Work-Reminder-App/static/js/runtime-main.378369fd.js"
+  }
+]);
